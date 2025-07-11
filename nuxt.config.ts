bb2b4943881fcc8e704e7c29d2e8467a3a21fc5a
Peter Lycos <plycos@gmail.com>
2025-07-10 22:14:20 -0400

    update configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
+  future: {
+    compatibilityVersion: 4,
+  },
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxt/ui']
+  modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxt/ui'],
+  css: ['~/assets/css/main.css'],
+  ui: {
+
+  }
 })
